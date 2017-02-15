ea16cf1adc9ff32e0b4bc4dc080b30c083e4865a
JY Kim <kimorkim@gmail.com>
2017-02-16 01:08:04 +0900

    불필요 소스 제거

--- src/main/appStartupManager.js
+++ src/main/appStartupManager.js
@@ -2,35 +2,6 @@ import { app } from 'electron';
 import path from 'path';
 import childProcess from 'child_process';
 
-export function createShortcut(locations, done) {
-  const updateExe = path.resolve(path.dirname(process.execPath), '..', 'Update.exe');
-  const target = path.basename(process.execPath);
-  const args = ['--createShortcut', target, '-l', locations];
-  const child = childProcess.spawn(updateExe, args, { detached: true });
-  child.on('close', () => {
-    if (done) {
-      done();
-    }
-  });
-}
-
-export function removeShortcut(locations, done) {
-  const updateExe = path.resolve(path.dirname(process.execPath), '..', 'Update.exe');
-  const target = path.basename(process.execPath);
-  const args = ['--removeShortcut', target, '-l', locations];
-  const child = childProcess.spawn(updateExe, args, { detached: true });
-  child.on('close', () => {
-    // const desktopEng = path.resolve(process.env.USERPROFILE, 'Desktop', 'Entry_HW.lnk');
-    // const startMenuEng = path.resolve(process.env.APPDATA,
-    // 'Microsoft', 'Windows', 'Start Menu', 'Programs', 'EntryLabs', 'Entry_HW.lnk');
-    // deleteRecursiveSync(desktopEng);
-    // deleteRecursiveSync(startMenuEng);
-    if (done) {
-      done();
-    }
-  });
-}
-
 export function handleStartupEvent() {
   if (process.platform !== 'win32') {
     return false;
